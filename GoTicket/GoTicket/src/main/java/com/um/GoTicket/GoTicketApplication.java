77237e993d7d265dd6e9ea0168532b240b0d41bd
ezequiel <mae.garcia@alumno.um.edu.ar>
2023-05-16 17:06:16 -0300

    GoTicket creation

---
+++ GoTicket/GoTicket/src/main/java/com/um/GoTicket/GoTicketApplication.java
@@ -0,0 +1,13 @@
+package com.um.GoTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoTicketApplication.class, args);
+	}
+
+}
