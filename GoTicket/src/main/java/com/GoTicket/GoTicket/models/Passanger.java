b2f18297d25d5fc6f31b5ed53cff748bef4711b9
EnzoFer <enzo.fernandez@hotmail.com>
2023-05-26 20:33:37 -0300

    add passanger, reservation and user models

--- GoTicket/src/main/java/com/GoTicket/GoTicket/models/Passanger.java
+++ GoTicket/src/main/java/com/GoTicket/GoTicket/models/Passanger.java
@@ -2,9 +2,41 @@ package com.GoTicket.GoTicket.models;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+
+import jakarta.persistence.Column;
+
 import java.io.Serializable;
 
 @Entity
 @Table(name = "passanger")
 public class Passanger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_passanger")
+    private Long id_passanger;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+
+    public Long getId_passanger() {
+        return id_passanger;
+    }
+
+    public void setId_passanger(Long id_passanger) {
+        this.id_passanger = id_passanger;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
--- GoTicket/src/main/java/com/GoTicket/GoTicket/models/Reservation.java
+++ GoTicket/src/main/java/com/GoTicket/GoTicket/models/Reservation.java
@@ -7,4 +7,21 @@ import java.io.Serializable;
 @Entity
 @Table(name = "reservation")
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_reservation")
+    private Long id_reservation;
+
+    
+
+
+    public Long getId_reservation() {
+        return id_reservation;
+    }
+
+    public void setId_reservation(Long id_reservation) {
+        this.id_reservation = id_reservation;
+    }
+
 }
+
