04882500ccedcab664d4576a12204815da782257
ezequiel <mae.garcia@alumno.um.edu.ar>
2023-05-26 20:35:22 -0300

    models trip and bus

--- GoTicket/src/main/java/com/GoTicket/GoTicket/models/Bus.java
+++ GoTicket/src/main/java/com/GoTicket/GoTicket/models/Bus.java
@@ -7,4 +7,16 @@ import java.io.Serializable;
 @Entity
 @Table(name = "bus")
 public class Bus implements Serializable{
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @column(name = "id_bus")
+    private Long id_bus;
+    @column(name = "capacity")
+    private int capacity;
+
+    public Long getId_bus() {return id_bus;}
+    public int getCapacity() {return capacity;}
+
+    public void setId_bus(Long id_bus) {this.id_bus = id_bus;}
+    public void setCapacity(int capacity) {this.capacity = capacity;}
 }
