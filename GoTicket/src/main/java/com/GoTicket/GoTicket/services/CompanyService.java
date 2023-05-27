e9c4f8843a09e128c3a4e3bfd690574cbfa3d938
Bruno Rosales <brunonicorc@hotmail.com>
2023-05-26 21:06:49 -0300

    finished service

---
+++ GoTicket/src/main/java/com/GoTicket/GoTicket/services/CompanyService.java
@@ -0,0 +1,27 @@
+package com.GoTicket.GoTicket.services;
+
+import com.GoTicket.GoTicket.models.Company;
+
+import java.util.List;
+
+public class CompanyService implements BaseService<Company> {
+    public List<Company> findAll() throws Exception {
+        return null;
+    }
+
+    public Company findById(Long id) throws Exception {
+        return null;
+    }
+
+    public Company save(Company entity) throws Exception {
+        return null;
+    }
+
+    public Company update(Long id, Company entity) throws Exception {
+        return null;
+    }
+
+    public boolean delete(Long id) throws Exception {
+        return false;
+    }
+}
---
+++ GoTicket/src/main/java/com/GoTicket/GoTicket/services/UserServices.java
@@ -0,0 +1,28 @@
+package com.GoTicket.GoTicket.services;
+
+import com.GoTicket.GoTicket.models.User;
+
+import java.util.List;
+
+public class UserServices implements BaseService<User>{
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public User findById(int id) {
+        return null;
+    }
+
+    @Override
+    public User save(User user) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+}
