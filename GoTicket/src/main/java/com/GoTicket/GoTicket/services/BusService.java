e25c5653c8b9b07bb0dd613541a5025aa1c1401e
ezequiel <mae.garcia@alumno.um.edu.ar>
2023-05-26 21:42:46 -0300

    repository and service

---
+++ GoTicket/src/main/java/com/GoTicket/GoTicket/services/BusService.java
@@ -0,0 +1,31 @@
+package com.GoTicket.GoTicket.services;
+
+import com.GoTicket.GoTicket.models.Bus;
+import java.util.List;
+
+public class BusService implements BaseService<Bus>{
+    @Override
+    public List<Bus> findAll() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Bus findById(Long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Bus save(Bus entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Bus update(Long id, Bus entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) throws Exception {
+        return false;
+    }
+}
---
+++ GoTicket/src/main/java/com/GoTicket/GoTicket/services/CityService.java
@@ -0,0 +1,32 @@
+package com.GoTicket.GoTicket.services;
+
+import com.GoTicket.GoTicket.models.City;
+
+import java.util.List;
+
+public class CityService  implements BaseService<City>{
+    @Override
+    public List<City> findAll() throws Exception {
+        return null;
+    }
+
+    @Override
+    public City findById(Long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public City save(City entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public City update(Long id, City entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) throws Exception {
+        return false;
+    }
+}
