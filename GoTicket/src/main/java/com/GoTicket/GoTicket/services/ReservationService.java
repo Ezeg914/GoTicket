876be78abaeeab6c21787e660464fb42fea45473
EnzoFer <enzo.fernandez@hotmail.com>
2023-05-27 16:19:58 -0300

    repository and service for passanger and reservation

---
+++ GoTicket/src/main/java/com/GoTicket/GoTicket/services/ReservationService.java
@@ -0,0 +1,27 @@
+package com.GoTicket.GoTicket.services;
+
+import com.GoTicket.GoTicket.models.Reservation;
+
+import java.util.List;
+
+public class ReservationService implements BaseService<Reservation> {
+    public List<Reservation> findAll() throws Exception {
+        return null;
+    }
+
+    public Reservation findById(Long id) throws Exception {
+        return null;
+    }
+
+    public Reservation save(Reservation entity) throws Exception {
+        return null;
+    }
+
+    public Reservation update(Long id, Reservation entity) throws Exception {
+        return null;
+    }
+
+    public boolean delete(Long id) throws Exception {
+        return false;
+    }
+}
