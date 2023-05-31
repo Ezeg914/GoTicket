0257ecb7009eb49858cf1a12751a07b8a6901262
ezequiel <mae.garcia@alumno.um.edu.ar>
2023-05-31 20:28:51 -0300

    service bus and city

--- GoTicket/src/main/java/com/GoTicket/GoTicket/services/CityService.java
+++ GoTicket/src/main/java/com/GoTicket/GoTicket/services/CityService.java
@@ -1,27 +1,66 @@
 package com.GoTicket.GoTicket.services;
 
 import com.GoTicket.GoTicket.models.City;
+import com.GoTicket.GoTicket.repositories.CityRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class CityService  implements BaseService<City>{
+
+    private CityRepository cityRepository;
+    public CityService(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
+    }
+
     @Override
+    @Transactional
     public List<City> findAll() throws Exception {
-        return null;
+        try{
+            List<City> models = cityRepository.findAll();
+            return models;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
     @Override
+    @Transactional
     public City findById(Long id) throws Exception {
-        return null;
+        try{
+            Optional<City> modelOptional = cityRepository.findById(id);
+            return modelOptional.get();
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
     @Override
+    @Transactional
     public City save(City entity) throws Exception {
-        return null;
+        try{
+            entity = cityRepository.save(entity);
+            return entity;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
     @Override
+    @Transactional
     public boolean delete(Long id) throws Exception {
-        return false;
+        try{
+            Optional<City> modelOptional = cityRepository.findById(id);
+            if(!modelOptional.isPresent()){
+                throw new Exception();
+            }
+            cityRepository.delete(modelOptional.get());
+            return true;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 }
