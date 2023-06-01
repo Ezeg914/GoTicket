4454e54e665d98eedff678af8c313b8c7c3b6fc0
EnzoFer <enzo.fernandez@hotmail.com>
2023-06-01 12:16:33 -0300

    services methods in the bus, passanger and reservation

--- GoTicket/src/main/java/com/GoTicket/GoTicket/services/BusService.java
+++ GoTicket/src/main/java/com/GoTicket/GoTicket/services/BusService.java
@@ -2,25 +2,73 @@ package com.GoTicket.GoTicket.services;
 
 import com.GoTicket.GoTicket.models.Bus;
 import java.util.List;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class BusService implements BaseService<Bus>{
+
+    private BusRepository busRepository;
+
+    public BusService(BusRepository busRepository) {
+        this.busRepository = busRepository;
+    }
+
+
+
     @Override
+    @Transactional
     public List<Bus> findAll() throws Exception {
-        return null;
+        try {
+            List<Bus> models = busRepository.findAll();
+            return models;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+
+
     }
 
     @Override
+    @Transactional
     public Bus findById(Long id) throws Exception {
-        return null;
+        try {
+            Optional<Bus> modelOptional = busRepository.findById(id);
+            return modelOptional.get();
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+        
     }
 
     @Override
-    public Bus save(Bus entity) throws Exception {
-        return null;
+    @Transactional
+    public Bus save(Bus model) throws Exception {
+        try {
+            model = busRepository.save(model);
+            return model;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+        
     }
 
     @Override
-    public boolean delete(Long id) throws Exception {
-        return false;
+    @Transactional
+    public boolean deleteById(Long id) throws Exception {
+        try {
+            if(busRepository.existsById(id)){
+                busRepository.deleteById(id);
+                return true;
+            } else {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
+
 }
