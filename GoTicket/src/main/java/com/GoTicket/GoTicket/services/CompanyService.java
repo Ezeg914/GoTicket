10f408408a683944bbbca9ce0ead17746f989849
EnzoFer <enzo.fernandez@hotmail.com>
2023-06-02 14:22:10 -0300

    company service

--- GoTicket/src/main/java/com/GoTicket/GoTicket/services/CompanyService.java
+++ GoTicket/src/main/java/com/GoTicket/GoTicket/services/CompanyService.java
@@ -1,27 +1,83 @@
 package com.GoTicket.GoTicket.services;
 
 import com.GoTicket.GoTicket.models.Company;
+import com.GoTicket.GoTicket.repositories.CompanyRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Trasactional;
 
+@Service
 public class CompanyService implements BaseService<Company> {
+    
+    private CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+        
+    @Override
+    @Transactional
     public List<Company> findAll() throws Exception {
-        return null;
+        try{
+            List<Company> models = companyRepository.findAll();
+            return models;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
+    @Override
+    @Transactional
     public Company findById(Long id) throws Exception {
-        return null;
+        try{
+            Optional<Company> modelOptional = companyRepository.findById(id);
+            return modelOptional.get();
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
-    public Company save(Company entity) throws Exception {
-        return null;
+
+    @Override
+    @Transactional
+    public Company save(Company model) throws Exception {
+        try{
+            model = companyRepository.save(model);
+            return model;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
-    public Company update(Long id, Company entity) throws Exception {
-        return null;
+
+    @Override
+    @Transactional
+    public Company update(Long id, Company model) throws Exception {
+        try{
+            Optional<Company> modelOptional = companyRepository.findById(id);
+            Company company = modelOptional.get();
+            company = companyRepository.save(company);
+            return company;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
+
+    @Override
+    @Transactional
     public boolean delete(Long id) throws Exception {
-        return false;
+        try{
+            if(companyRepository.existsById(id)){
+                companyRepository.deleteById(id);
+                return true;
+            }else{
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 }
