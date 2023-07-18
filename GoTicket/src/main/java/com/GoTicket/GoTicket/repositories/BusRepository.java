3842d8620897c2ad50c7f62828868d1a882856a8
Bruno Rosales <brunonicorc@hotmail.com>
2023-07-18 17:20:54 -0300

    run api and create user finished

--- GoTicket/src/main/java/com/GoTicket/GoTicket/repositories/BusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.GoTicket.GoTicket.repositories;
-
-import com.GoTicket.GoTicket.models.Bus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BusRepository extends JpaRepository<Bus, Long> {
-}
