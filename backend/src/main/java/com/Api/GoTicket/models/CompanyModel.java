af7737d9d216a52972fc59f66866b20a73c455ed
bruno <brunonicorc@hotmail.com>
2023-08-08 12:07:28 -0300

    finished bus,company and trip

--- backend/src/main/java/com/Api/GoTicket/models/CompanyModel.java
+++ backend/src/main/java/com/Api/GoTicket/models/CompanyModel.java
@@ -1,7 +1,13 @@
 package com.Api.GoTicket.models;
 
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Entity
 @Table(name = "company")
@@ -13,6 +19,13 @@ public class CompanyModel {
     @Column
     private String name;
 
+    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
+    private List<BusModel> buses = new ArrayList<BusModel>();
+
+    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
+    private List<TripModel> trips = new ArrayList<TripModel>();
+
+    // Getters and Setters
     public long getId(){
         return id;
     }
@@ -21,6 +34,14 @@ public class CompanyModel {
         this.id = id;
     }
 
+    @JsonIgnoreProperties({"company"})
+    public List<BusModel> getBuses(){
+        return buses;
+    }
+
+    public void setBuses(List<BusModel> buses){
+        this.buses = buses;
+    }
     public String getName(){
         return name;
     }
@@ -29,4 +50,11 @@ public class CompanyModel {
         this.name = name;
     }
 
+    @JsonIgnoreProperties({"company"})
+    public List<TripModel> getTrips(){
+        return trips;
+    }
+    public void setTrips(List<TripModel> trips){
+        this.trips = trips;
+    }
 }
